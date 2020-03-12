b8501b407cf82cc9f621d3b0293b786982acdeb4
Ty Raino <mstymetris@yahoo.com>
2020-03-12 04:35:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
 new_array []
   sentence.split
-  sentence.each do |words|.reverse
-    new_array << words
+  sentence.each do |words|
+    new_array << words.reverse
 end
